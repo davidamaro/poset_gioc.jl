64a0b0aea8417e54983d39a01f761b6b19ab2b5c
David Amaro Alcalá <dav1494@ciencias.unam.mx>
2020-10-05 12:01:37 -0500

    error bobo causado por el maluso de git.

--- src/Posets.jl
+++ src/Posets.jl
@@ -29,11 +29,7 @@ import .Generic:  energia_por_pasos_p, energia_adhoc, energia_local,
                   derecha_abajo,
                   isacyclic, caminata_poset, listaposetsaleatorios,
                   graficacolor, encontrarminimo, posicionpromedio, posicionvarianza, pearson,
-<<<<<<< HEAD
                   caminatale, sensibilidad, generarmatriz, iteraciontransitiva, convertidor
-=======
-                  caminatale, sensibilidad, generarmatriz, iteraciontransitiva
->>>>>>> eb17b66... nuevo metodo para la iteracion transitiva relacionado con el fuzzy poset.
 
 export energia_por_pasos_p, energia_adhoc, energia_local
 export energia_no_local, energia_trucada, energia_por_pasos
